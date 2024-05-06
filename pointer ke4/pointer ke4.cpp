acff46a0e808b68487524c21d33eb31df1afd3f1
feraistanti <fistanti551@gmail.com>
2024-05-06 15:45:45 +0700

    pointer yang ke 4 selesai

---
+++ pointer ke4/pointer ke4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class pelajar;
+class manusia
+{
+public:
+	void showNilaipelajar(pelajar& x);
+};
+
+class pelajar
+{
+private:
+	int nilai;
+
+public:
+	pelajar() { nilai = 100; }
+	friend void manusia::showNilaipelajar(pelajar& x);
+};
+
+void manusia::showNilaipelajar(pelajar& x)
+{
+	std::cout << x.nilai;
+}
